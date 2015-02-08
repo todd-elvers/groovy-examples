e43d6a276bcbe7db65ed15a1441988f889d482d1
Todd Elvers <todd.elvers@gmail.com>
2015-02-08 17:07:14 -0600

    Clarified Trait example

--- src/main/groovy/newInGroovy2_3/Traits.groovy
+++ src/main/groovy/newInGroovy2_3/Traits.groovy
@@ -13,16 +13,11 @@ trait DrivingAbility {
     // Abstract method must be implemented in class implementing this trait
     abstract int numberOfWheels()
 
+    // Traits only support public and private methods
+    // Private methods will not be visible to the implementing class
     String drive() {
         // 'this' represents the implementing instance, not the trait
-        String classCallingMethod = determineClassName(this.class.toString())
-        "This ${classCallingMethod} can drive and has ${numberOfWheels()} wheels."
-    }
-
-    // Traits only support public and private methods
-    // This method will not be available to the class implementing this trait
-    private determineClassName(String classNameWithPackage){
-        classNameWithPackage.tokenize('.')[-1]
+        "This ${this.class.simpleName} can drive and has ${numberOfWheels()} wheels."
     }
 }
 
@@ -33,5 +28,5 @@ class Car implements DrivingAbility {
 }
 
 def car = new Car()
-println car.drive()
+assert car.drive() == "This Car can drive and has 4 wheels."
 
